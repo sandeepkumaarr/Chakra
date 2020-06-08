257b040463f4815afac257be2e6717e74a7e7cda
Sandeep Kumar <kumar.sandeep648@gmail.com>
2020-06-08 17:22:04 +0530

    test

--- pages/_app.js
+++ pages/_app.js
@@ -5,11 +5,14 @@ import {
 	ColorModeProvider,
 } from '@chakra-ui/core';
 import Navbar from '../components/Navbar';
+import DndProvider from 'react-dnd';
+import Backend from 'react-dnd-html5-backend';
 
 function MyApp({ Component, pageProps }) {
 	return (
 		<ThemeProvider>
 			<ColorModeProvider>
+				<DndProvider backend={Backend}>
 				<Flex direction='column' align='center' justify='center'>
 					<CSSReset />
 					<Navbar />
@@ -17,6 +20,7 @@ function MyApp({ Component, pageProps }) {
 						<Component {...pageProps} />
 					</Flex>
 				</Flex>
+				</DndProvider>
 			</ColorModeProvider>
 		</ThemeProvider>
 	);
